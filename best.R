ae74299f08c118f9d46bf1578a34a030f0e400c7
Anuj Parashar <promisinganuj@gmail.com>
2015-04-21 22:38:10 +0530

    Update best.R
    Assigned character class to columns while reading the dataframe using "colClasses" parameter.

--- best.R
+++ best.R
@@ -1,35 +1,28 @@
 best <- function (state, outcome){
     result <- data.frame()
     # Reading the outcome file
-    outcomeDF <- read.csv('outcome-of-care-measures.csv')
+    outcomeDF <- read.csv('outcome-of-care-measures.csv', colClasses = "character")
     # Getting state list
     statelist <- unique(outcomeDF[,7])
     
     # Validations
-    if (!(state %in% statelist)){
-        stop("invalid state")
-    }
-    if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
-        stop("invalid outcome")
-    }
-    
+    if (!(state %in% statelist)) stop("invalid state")
+
+#    if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) stop("invalid outcome")
     # Getting the column to be read
     coltoread <- if (outcome == "heart attack")  11
             else if (outcome == "heart failure") 17
             else if (outcome == "pneumonia")     23
+            else                                 stop("invalid outcome")
 
     # Getting the state and corresponding outcome value, data filtered by state and not NA outcome
     result <- subset(outcomeDF[, c(2,coltoread)], outcomeDF[coltoread] != "Not Available" & outcomeDF[7] == state)
 
-    # Converting data types for individual columns from the default "factor" datatype
-    result[,1] <- as.character(result[,1])
-    result[,2] <- as.numeric(paste(result[,2])) # Paste is required to avoid decimal truncation
+    # Converting outcome values to numeric for sorting purpose
+    result[,2] <- as.numeric(result[,2])
 
     # Getting the sorted result based on outcome and then on hospital
     final <- result[order (result[2], result[1]), ]
-    
-    # Unlink the dataframes
-    unlink(outcomeDF)
 
     # Print the hospital name (first row, first column)
     final[1,1]
